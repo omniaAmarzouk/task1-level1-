0c2460fef64d840ef13d9a59c1cc80eba3b465c6
omniaAmarzouk <141080839+omniaAmarzouk@users.noreply.github.com>
2024-02-02 18:06:47 +0200

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,14 +1,5 @@
 ﻿/*● For each expression:
-○ Step-by-step, manually simplify these until they are simply T or F
-○ Optional: write a line of code to verify your result
-● T && T && F && T
-● T && T && F && T || T && T
-● T && T && T && T || T && (T || F)
-● T && T && T || T && (F || (T && (T && T)))
-● T && T || T && F && T || T && T && F || (T && (T || F))
-● T && T || T && F && T || (T && T && F || (T && (T || F)))
-● (T && T || T && F && T || T) && T && F || (T && (T || F))
-● T && T || T && (F && T || T && T) && F || (T && (T || F))*/
+
 
 #include <iostream>
 using namespace std;
