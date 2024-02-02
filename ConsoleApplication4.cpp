f73e4577af74111b0e25fc9eaa3677f71694e2cd
omniaAmarzouk <141080839+omniaAmarzouk@users.noreply.github.com>
2024-02-02 18:07:27 +0200

    Update ConsoleApplication4.cpp

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,10 +1,4 @@
 ﻿/*Problem #2: Fractional Part
-● Write a program that reads in 2 real numbers, a and b, and then divides
-them (a/b), but prints out only the fraction part
-● Input: 201 25
-● Output: 0.04
-○ Notice: 201 / 25 = 8.04
-○ We only want the fraction part: 0.04*/
 
 #include <iostream>
 using namespace std;
