ee4088f1b0b3e6545ade247d95bb98318dc56fca
omniaAmarzouk <141080839+omniaAmarzouk@users.noreply.github.com>
2024-02-02 18:06:22 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,15 +1,5 @@
 ﻿/*  Homework 2: Create your own Logic!
-● Write a program that reads in integers for 3 classroom-related variables:
-○ Number of boys (nb), number of girls (ng), number of teachers (nt)
-● Prepare and print a boolean variable for these cases:
-● nb greater than 25
-● ng less than or equal to 30
-● nb > 20 and nt > 2, or ng > 30 and nt > 4
-● Either nb < 60 or ng < 70
-● Neither nb >= 60 nor ng >= 70
-● nb is 10 more than ng
-● The difference between nb and ng is more than 10 or nt > 5
-● Either nb is 10 more than ng, or ng is 15 more than nb*/
+
 #include <iostream>
 using namespace std;
 
