ff82ecc037c98c662a904b0064f4f3276ee54131
omniaAmarzouk <141080839+omniaAmarzouk@users.noreply.github.com>
2024-02-02 18:07:11 +0200

    Update ConsoleApplication3.cpp

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,13 +1,4 @@
 ﻿/*Problem #1: Averages
-● Write a program that reads in 5 real numbers and prints out the following:
-○ A) Their average
-○ B) The sum of the first 3 numbers divided by the sum of the last 2 numbers
-○ C) The average of the first 3 numbers divided by the average of the last 2 numbers
-○ What is the mathematical relationship between B and C?
-● Expected results from the input 1 2 3 4 5
-○ 3
-○ 0.666666667
-○ 0.444444444*/
 
 #include <iostream>
 using namespace std;
